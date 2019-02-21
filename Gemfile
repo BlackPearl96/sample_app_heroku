5b00bb3783229daa2cdc553ab16197b41d4f6066
BlackPearl96 <van9665@gmail.com>
2019-02-21 16:07:15 +0700

    finish chapter_10

--- Gemfile
+++ Gemfile
@@ -8,8 +8,10 @@ gem "active_attr"
 gem "bcrypt", "3.1.12"
 gem "bootsnap", ">= 1.1.0", require: false
 gem "bootstrap-sass", "3.3.7"
+gem "bootstrap-will_paginate", "1.0.0"
 gem "coffee-rails", "~> 4.2"
 gem "config"
+gem "faker", "1.7.3"
 gem "jbuilder", "~> 2.5"
 gem "jquery-rails"
 gem "puma", "~> 3.11"
@@ -20,6 +22,7 @@ gem "sass-rails", "~> 5.0"
 gem "sqlite3", "~> 1.3.6"
 gem "turbolinks", "~> 5"
 gem "uglifier", ">= 1.3.0"
+gem "will_paginate", "3.1.6"
 
 group :production do
   gem "pg", "0.20.0"
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_21_062302) do
-
+ActiveRecord::Schema.define(version: 2019_02_23_061819) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -19,7 +18,7 @@ ActiveRecord::Schema.define(version: 2019_02_21_062302) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
-
 end
